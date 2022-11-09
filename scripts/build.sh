1e081cc8ede02768054629e9fa726423f5c3e103
CowKeyMan <dancauchi1@gmail.com>
2022-11-09 02:20:44 +0200

    Add benchmark and method to build the benchmarks

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,45 @@
+#!/bin/bash
+
 mkdir -p build
 cd build
 cmake \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-  -DCMAKE_BUILD_TYPE=Debug \
+  -DCMAKE_BUILD_TYPE=Release \
   -DENABLE_CLANG_TIDY=ON \
   -DENABLE_HEADER_GUARDS_CHECK=ON \
   -DENABLE_CLANG_FORMAT_CHECK=ON \
   -DKSEQPP_READ_BUILD_TESTS=ON \
-  -DKSEQPP_READ_BUILD_TESTS=ON \
+  -DKSEQPP_READ_BUILD_BENCHMARKS=ON \
   ..
 cmake --build . -j8
+cd ..
+
+
+# download benchmark_objects
+mkdir -p benchmark_objects
+cd benchmark_objects
+
+# Download Fasta
+FASTA="FASTA.fna"
+## Source: https://www.ncbi.nlm.nih.gov/projects/genome/guide/human/index.shtml
+wget -nc "https://ftp.ncbi.nlm.nih.gov/refseq/H_sapiens/annotation/GRCh38_latest/refseq_identifiers/GRCh38_latest_genomic.fna.gz" -O "${FASTA}.gz"
+if test -f "${FASTA}"; then
+  echo "${FASTA} already unzipped, skipping "
+else
+  echo "Unzipping ${FASTA}"
+  gzip -dkf "${FASTA}.gz" > "${FASTA}"
+fi
+
+# Download Fastq
+FASTQ="FASTQ.fnq"
+## Source: https://www.ebi.ac.uk/ena/browser/view/SRX11174563?show=reads
+wget -nc "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR148/030/SRR14856430/SRR14856430_1.fastq.gz" -O "${FASTQ}.gz"
+if test -f "${FASTQ}"; then
+  echo "${FASTQ} already unzipped, skipping "
+else
+  echo "Unzipping ${FASTQ}"
+  gzip -dkf "${FASTQ}.gz" > "${FASTQ}"
+fi
+
 cd ..
 
---
+++ src/benchmark.cpp
@@ -0,0 +1,103 @@
+#include <chrono>
+std::chrono::system_clock::time_point TIME_IT_START_TIME, TIME_IT_END_TIME;
+unsigned long long TIME_IT_TOTAL = 0;
+#define TIME_IT(code_block)                                              \
+  TIME_IT_START_TIME = std::chrono::high_resolution_clock::now();        \
+  code_block;                                                            \
+  TIME_IT_END_TIME = std::chrono::high_resolution_clock::now();          \
+  TIME_IT_TOTAL = std::chrono::duration_cast<std::chrono::milliseconds>( \
+                    TIME_IT_END_TIME - TIME_IT_START_TIME                \
+  )                                                                      \
+                    .count();
+
+#include <iostream>
+
+#include "kseq++/seqio.hpp"
+#include "kseqpp_read.hpp"
+using std::cout;
+using std::endl;
+
+auto benchmark_reklibpp_fasta() -> void;
+auto benchmark_reklibpp_fastq() -> void;
+auto benchmark_klibpp_fasta() -> void;
+auto benchmark_klibpp_fastq() -> void;
+
+const int iterations = 5;
+
+int main() {
+  benchmark_reklibpp_fasta();
+  cout << endl;
+  benchmark_reklibpp_fastq();
+  cout << endl;
+  benchmark_klibpp_fasta();
+  cout << endl;
+  benchmark_klibpp_fastq();
+}
+
+auto benchmark_reklibpp_fasta() -> void {
+  double total = 0;
+  for (int i = 0; i < iterations + 1; ++i) {
+    auto record = reklibpp::Seq(16 * 1024 * 1024);
+    auto ssi = reklibpp::SeqStreamIn("benchmark_objects/FASTA.fna");
+    TIME_IT({
+      while (ssi >> record) { record.clear(); }
+    })
+    if (i > 0) {
+      cout << "reklibpp fasta iteration " << i << ": " << TIME_IT_TOTAL << "ms"
+           << endl;
+      total += double(TIME_IT_TOTAL) / iterations;
+    }
+  }
+  cout << "Average: " << total << "ms" << endl;
+}
+
+auto benchmark_reklibpp_fastq() -> void {
+  double total = 0;
+  for (int i = 0; i < iterations + 1; ++i) {
+    auto record = reklibpp::Seq(16 * 1024 * 1024);
+    auto ssi = reklibpp::SeqStreamIn("benchmark_objects/FASTQ.fnq");
+    TIME_IT({
+      while (ssi >> record) { record.clear(); }
+    })
+    if (i > 0) {
+      cout << "reklibpp fastq iteration " << i << ": " << TIME_IT_TOTAL << "ms"
+           << endl;
+      total += double(TIME_IT_TOTAL) / iterations;
+    }
+  }
+  cout << "Average: " << total << "ms" << endl;
+}
+
+auto benchmark_klibpp_fasta() -> void {
+  double total = 0;
+  for (int i = 0; i < iterations + 1; ++i) {
+    auto record = klibpp::KSeq();
+    auto ssi = klibpp::SeqStreamIn("benchmark_objects/FASTA.fna");
+    TIME_IT({
+      while (ssi >> record) {}
+    })
+    if (i > 0) {
+      cout << "klibpp fastq iteration " << i << ": " << TIME_IT_TOTAL << "ms"
+           << endl;
+      total += double(TIME_IT_TOTAL) / iterations;
+    }
+  }
+  cout << "Average: " << total << "ms" << endl;
+}
+
+auto benchmark_klibpp_fastq() -> void {
+  double total = 0;
+  for (int i = 0; i < iterations + 1; ++i) {
+    auto record = klibpp::KSeq();
+    auto ssi = klibpp::SeqStreamIn("benchmark_objects/FASTQ.fnq");
+    TIME_IT({
+      while (ssi >> record) {}
+    })
+    if (i > 0) {
+      cout << "klibpp fastq iteration " << i << ": " << TIME_IT_TOTAL << "ms"
+           << endl;
+      total += double(TIME_IT_TOTAL) / iterations;
+    }
+  }
+  cout << "Average: " << total << "ms" << endl;
+}
