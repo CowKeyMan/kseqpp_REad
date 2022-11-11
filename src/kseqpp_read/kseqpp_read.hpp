8db86917dc1644d7287a88d68792fdb87ab13242
CowKeyMan <dancauchi1@gmail.com>
2022-11-11 20:26:41 +0200

    Make kseqpp_read pass FASTA tests

--- src/kseqpp_read/kseqpp_read.hpp
+++ src/kseqpp_read/kseqpp_read.hpp
@@ -6,6 +6,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <ios>
+#include <stdexcept>
 #include <string>
 #include <vector>
 #include <zlib.h>
@@ -15,23 +16,40 @@ using std::vector;
 
 namespace reklibpp {
 
-using size_type = uint64_t;
+using size_t = uint64_t;
 
 class Seq {  // kseq_t
   public:
-    Seq(size_type max_seq_size) {
+    Seq(size_t max_seq_size) {
       this->max_seq_size = max_seq_size;
       this->seq.reserve(max_seq_size);
     }
-    size_type max_seq_size;
-    vector<size_type> string_breaks;
+
+    Seq(
+      const string seq,
+      const vector<size_t> string_breaks,
+      size_t max_seq_size = 16 * 1024
+    ) {
+      this->seq = seq;
+      this->string_breaks = string_breaks;
+      this->max_seq_size = max_seq_size;
+    }
+
+    size_t max_seq_size;
+    vector<size_t> string_breaks;
     string seq;
 
     inline void clear() {
       string_breaks.clear();
       seq.clear();
     }
+
+    bool operator==(const Seq &other) const {
+      return seq == other.seq && string_breaks == other.string_breaks;
+    }
 };
+
+constexpr static std::make_unsigned_t<size_t> DEFAULT_BUFSIZE = 16 * 1024;
 
 template <typename TFile, typename TFunc>
 class KStream {  // kstream_t
@@ -45,49 +63,53 @@ class KStream {  // kstream_t
       SPACE = 0,  // isspace(): \t, \n, \v, \f, \r
       LINE = 2  // line separator: "\n" (Unix) or "\r\n" (Windows)
     };
-    /* Consts */
-    constexpr static std::make_unsigned_t<size_type> DEFAULT_BUFSIZE
-      = 16384;  // 16kB buffer
     /* Data members */
     char *buf; /**< @brief character buffer */
-    size_type bufsize; /**< @brief buffer size */
-    size_type begin; /**< @brief begin buffer index */
-    size_type buf_end; /**< @brief end buffer index or error flag if -1 */
-    size_type current_seq_size; /**< @brief size of the sequence being read */
+    size_t bufsize; /**< @brief buffer size */
+    size_t buf_begin; /**< @brief begin buffer index */
+    size_t buf_end; /**< @brief end buffer index or error flag if -1 */
+    size_t current_seq_size; /**< @brief size of the sequence being read */
     bool is_eof; /**< @brief eof flag */
     bool is_tqs; /**< @brief truncated quality string flag */
-    bool is_ready; /**< @brief next record ready flag */
+    bool first_header_char_read; /**< @brief next record ready flag */
     bool finished_reading_seq; /**< @brief Sequence is done reading */
     bool has_read_something; /**< @brief Current seq contains something */
-    size_type qual_size; /**< @brief quality string size */
-    TFile f; /**< @brief file handler */
-    TFunc func; /**< @brief read function */
-    close_type close; /**< @brief close function */
+    char next_char; /** @brief A character to add at start of next sequence */
+    size_t qual_size; /**< @brief quality string size */
+    TFile file_handle; /**< @brief file handler */
+    TFunc load_buf; /**< @brief read function */
+    close_type close_func; /**< @brief close function */
   public:
     KStream(
-      TFile f_,
-      TFunc func_,
-      std::make_unsigned_t<size_type> bs_ = DEFAULT_BUFSIZE,
-      close_type cfunc_ = nullptr
+      TFile file_handle_,
+      TFunc load_bufile_handle_,
+      std::make_unsigned_t<size_t> bs_ = DEFAULT_BUFSIZE,
+      close_type close_func_ = nullptr
     )  // ks_init
         :
         buf(new char[bs_]),
         bufsize(bs_),
-        f(std::move(f_)),
-        func(std::move(func_)),
-        close(cfunc_) {
-      this->begin = 0;
+        file_handle(std::move(file_handle_)),
+        load_buf(std::move(load_bufile_handle_)),
+        close_func(close_func_) {
+      this->buf_begin = 0;
       this->buf_end = 0;
       this->qual_size = 0;
       this->is_eof = false;
       this->is_tqs = false;
-      this->is_ready = false;
+      this->first_header_char_read = false;
       this->finished_reading_seq = true;
       this->current_seq_size = 0;
+      this->next_char = 0;
     }
 
-    KStream(TFile f_, TFunc func_, close_type cfunc_):
-        KStream(std::move(f_), std::move(func_), DEFAULT_BUFSIZE, cfunc_) {}
+    KStream(TFile file_handle_, TFunc load_buf_, close_type close_func_):
+        KStream(
+          std::move(file_handle_),
+          std::move(load_buf_),
+          DEFAULT_BUFSIZE,
+          close_func_
+        ) {}
 
     KStream(KStream const &) = delete;
     KStream &operator=(KStream const &) = delete;
@@ -96,17 +118,18 @@ class KStream {  // kstream_t
       this->buf = other.buf;
       other.buf = nullptr;
       this->bufsize = other.bufsize;
-      this->begin = other.begin;
+      this->buf_begin = other.buf_begin;
       this->buf_end = other.buf_end;
       this->qual_size = other.qual_size;
       this->is_eof = other.is_eof;
       this->is_tqs = other.is_tqs;
-      this->is_ready = other.is_ready;
-      this->f = std::move(other.f);
-      this->func = std::move(other.func);
-      this->close = other.close;
+      this->first_header_char_read = other.first_header_char_read;
+      this->file_handle = std::move(other.file_handle);
+      this->load_buf = std::move(other.load_buf);
+      this->close_func = other.close_func;
       this->finished_reading_seq = other.finished_reading_seq;
       this->current_seq_size = other.current_seq_size;
+      this->next_char = other.next_char;
     }
 
     KStream &operator=(KStream &&other) noexcept {
@@ -115,23 +138,24 @@ class KStream {  // kstream_t
       this->buf = other.buf;
       other.buf = nullptr;
       this->bufsize = other.bufsize;
-      this->begin = other.begin;
+      this->buf_begin = other.buf_begin;
       this->buf_end = other.buf_end;
       this->qual_size = other.qual_size;
       this->is_eof = other.is_eof;
       this->is_tqs = other.is_tqs;
-      this->is_ready = other.is_ready;
-      this->f = std::move(other.f);
-      this->func = std::move(other.func);
-      this->close = other.close;
+      this->first_header_char_read = other.first_header_char_read;
+      this->file_handle = std::move(other.file_handle);
+      this->load_buf = std::move(other.load_buf);
+      this->close_func = other.close_func;
       this->finished_reading_seq = other.finished_reading_seq;
       this->current_seq_size = other.current_seq_size;
+      this->next_char = other.next_char;
       return *this;
     }
 
     ~KStream() noexcept {
       delete[] this->buf;
-      if (this->close != nullptr) this->close(this->f);
+      if (this->close_func != nullptr) this->close_func(this->file_handle);
     }
     /* Methods */
     inline bool err() const {  // ks_err
@@ -139,142 +163,192 @@ class KStream {  // kstream_t
     }
 
     inline bool eof() const {  // ks_eof
-      return this->is_eof && this->begin >= this->buf_end;
+      return this->is_eof && this->buf_begin >= this->buf_end;
     }
 
     inline bool tqs() const { return this->is_tqs; }
 
     inline bool fail() const {
-      return this->err() || this->tqs()
-          || (this->eof() && !has_read_something);
+      return this->err() || this->tqs() || (this->eof() && !has_read_something);
     }
 
     inline KStream &operator>>(Seq &rec) {
-      char c;
+      char c = -1;
       this->has_read_something = false;
       while (true) {
-        if (this->finished_reading_seq) {
-          this->current_seq_size = 0;
-          this->finished_reading_seq = false;
-          if (!this->is_ready) {  // then jump to the next header line
-            while ((c = this->getc()) && c != '>' && c != '@') {}
-            if (this->fail()) return *this;
-            this->is_ready = true;
-          }  // else: the first header char has been read in the previous call
-          if (!this->getuntil(SEP::SPACE, nullptr, &c))
-            return *this;  // read name
-          if (c != '\n') {
-            this->getuntil(SEP::LINE, nullptr, nullptr);  // read comment
-          }
-        }
+        if (this->finished_reading_seq) { this->read_header(); }
+        if (this->fail()) { return *this; }
 
-        // read Sequence
-        size_type previous_length = rec.seq.size();
-        while (rec.seq.size() < rec.max_seq_size - 1 && (c = this->getc())
-               && c != '>' && c != '@' && c != '+') {
-          if (c == '\n') continue;  // skip empty lines
-          rec.seq += c;
-          // read the rest of the line
-          this->getuntil(
-            SEP::LINE, &rec.seq, &c, nullptr, rec.max_seq_size - 1
-          );
-          this->has_read_something = true;
-        }
-        this->finished_reading_seq
-          = c == '>' || c == '@' || c == '+' || std::isspace(c) || this->eof();
-        this->current_seq_size += rec.seq.size() - previous_length;
-        if (this->finished_reading_seq) {
-          rec.string_breaks.push_back(rec.seq.size() - 1);
-        } else {
-          rec.seq += c;
-          ++this->current_seq_size;
-          return *this;
-        }
-        // the first header character has been read
-        if (c == '>' || c == '@') this->is_ready = true;
+        if (read_sequence(rec)) { break; }
+        if (this->eof()) { return *this; }
 
         // Read + line if FASTQ
-        if (c != '+') continue;  // FASTA
-        while ((c = this->getc()) && c != '\n') {}  // skip the rest of '+' line
+        if (c != '+') continue;  // FASTA if no +
+        this->skip_to_next_line();
 
         // Read Quality string line
-        if (this->eof()) {  // error: no quality string
-          this->is_tqs = true;
-          return *this;
-        }
-        while (this->getuntil(SEP::LINE, nullptr, nullptr, &this->qual_size)
-               && this->qual_size < this->current_seq_size) {}
-        if (this->err()) return *this;
-        this->is_ready = false;  // we have not come to the next header line
-        if (this->current_seq_size != this->qual_size) {
-          this->is_tqs = true;  // error: qual string is of a different length
-          return *this;
-        }
-        this->qual_size = 0;
+        this->read_quality_string();
+        if (this->is_tqs || this->err()) { return *this; }
+        this->first_header_char_read = false;
+
         if (this->eof()) { return *this; }
       }
+      return *this;
+    }
+
+    inline bool read_sequence(Seq &rec) {
+      char c;
+      size_t previous_length = rec.seq.size();
+      while (c = this->next_char ? next_char : this->getc()) {
+        if (c == '\n' || c == '\r') {
+          next_char = 0;
+          continue;
+        } else if (is_ready_char(c) || c == '+') {
+          break;
+        }
+        if (rec.seq.size() == rec.max_seq_size) {
+          this->next_char = c;
+          return true;
+        } else {
+          rec.seq += c;
+          this->next_char = 0;
+        }
+        this->getuntil(
+          SEP::LINE, &rec.seq, &this->next_char, nullptr, rec.max_seq_size
+        );
+        this->has_read_something = rec.seq.size() > 0;
+      }
+      this->current_seq_size += rec.seq.size() - previous_length;
+      this->first_header_char_read = is_ready_char(c);
+      this->finished_reading_seq = is_ready_char(c) || c == '+' || this->eof();
+      if (this->finished_reading_seq) {
+        rec.string_breaks.push_back(rec.seq.size() - 1);
+      }
+      return false;
+    }
+
+    inline void read_header() {
+      char last_char;
+      this->current_seq_size = 0;
+      this->finished_reading_seq = false;
+      if (!this->first_header_char_read) {
+        this->read_until_next_ready_char();
+        if (this->fail()) return;
+        this->first_header_char_read = true;
+      }
+      if (!this->read_name(last_char)) return;
+      if (last_char != '\n') { this->read_comment(); }
+    }
+
+    inline void read_until_next_ready_char() {
+      char c;
+      while ((c = this->getc()) && !is_ready_char(c)) {}
+    }
+
+    inline bool read_name(char &last_char) {
+      return this->getuntil(SEP::SPACE, nullptr, &last_char);
+    }
+
+    inline bool read_comment() {
+      return this->getuntil(SEP::LINE, nullptr, nullptr);
+    }
+
+    inline bool is_ready_char(char c) { return c == '>' || c == '@'; }
+
+    inline void skip_to_next_line() {
+      char c;
+      while ((c = this->getc()) && c != '\n') {}
+    }
+
+    inline void read_quality_string() {
+      if (this->eof()) {  // no quality string
+        this->is_tqs = true;
+        return;
+      }
+      size_t qual_size;
+      while (this->getuntil(SEP::LINE, nullptr, nullptr, &qual_size)
+             && this->qual_size < this->current_seq_size) {}
+      if (this->err()) { return; }
+      if (this->current_seq_size != this->qual_size) { this->is_tqs = true; }
     }
 
     operator bool() const { return !this->fail(); }
 
     /* Low-level methods */
-    inline char getc() noexcept {  // ks_getc
-      if (this->err() || this->eof()) return 0;  // error
-      if (this->begin >= this->buf_end) {  // fetch
-        this->begin = 0;
-        this->buf_end = this->func(this->f, this->buf, this->bufsize);
-        if (this->buf_end <= 0) {  // err if end == -1 and eof if 0
-          this->is_eof = true;
-          return 0;
-        }
+    inline char getc() noexcept {
+      if (this->err() || this->eof()) { return 0; }
+      if (this->buf_begin >= this->buf_end) {
+        this->fetch_buffer();
+        this->check_eof();
+        if (this->is_eof) { return 0; }
       }
-      // ready
-      return this->buf[this->begin++];
+      return this->buf[this->buf_begin++];
+    }
+
+    inline void fetch_buffer() noexcept {
+      this->buf_begin = 0;
+      this->buf_end
+        = this->load_buf(this->file_handle, this->buf, this->bufsize);
+    }
+
+    inline void check_eof() {
+      if (this->buf_end <= 0) { this->is_eof = true; }
     }
 
     inline bool getuntil(
       const SEP delimiter,
       std::string *str,
       char *last_char,
-      size_type *str_size = nullptr,
-      const size_type max_str_size = -1  // max value
+      size_t *str_size = nullptr,
+      const size_t max_str_size = ULLONG_MAX
     ) noexcept {
       bool gotany = false;
       if (last_char != nullptr) *last_char = 0;
-      size_type i = -1;
+      size_t i = ULLONG_MAX;
       char c = -1;
       do {
         if (!(c = this->getc())) break;
-        --this->begin;
-        // find index i where there is the delimeter or until the buffer's end
-        if (delimiter == SEP::LINE) {
-          for (i = this->begin;
-               i < this->buf_end && this->buf[i] != '\n'
-               && (str == nullptr || str->size() + (i - this->begin) < max_str_size);
-               ++i) {}
-        } else {  // delimiter == SEP::SPACE
-          for (i = this->begin;
-               i < this->buf_end && !std::isspace(this->buf[i]);
-               ++i) {}
-        }
+        --this->buf_begin;
+        i = this->get_idx_at_delim_or_bufend(delimiter, str, max_str_size);
         gotany = true;
-        if (str != nullptr) {
-          str->append(this->buf + this->begin, i - this->begin);
-        }
-        if (str_size != nullptr) {
-          *str_size += i - this->begin;
-        }
+        this->append_to_string(str, str_size, i - this->buf_begin);
+        this->buf_begin = i + 1;
         if (i > 0) { c = this->buf[i - 1]; }
-        this->begin = i + 1;
       } while (i >= this->buf_end);
       if (this->err() || (this->eof() && !gotany)) return false;
-      assert(i != -1);
       if (!this->eof() && last_char != nullptr) *last_char = this->buf[i];
       if (delimiter == SEP::LINE && c == '\r') {
-        if (str != nullptr) { str->pop_back(); }
-        if (str_size != nullptr) { *str_size -= 1; }
+        remove_last_element(str, str_size);
       }
       return true;
+    }
+
+    inline size_t get_idx_at_delim_or_bufend(
+      SEP delimiter, string *str, const size_t max_str_size
+    ) {
+      size_t i;
+      if (delimiter == SEP::LINE) {
+        for (i = this->buf_begin;
+             i < this->buf_end && this->buf[i] != '\n'
+             && (str == nullptr || str->size() + (i - this->buf_begin) < max_str_size);
+             ++i) {}
+      } else {  // delimiter == SEP::SPACE
+        for (i = this->buf_begin;
+             i < this->buf_end && !std::isspace(this->buf[i]);
+             ++i) {}
+      }
+      return i;
+    }
+
+    inline void
+    append_to_string(string *str, size_t *str_size, const size_t amount) {
+      if (str != nullptr) { str->append(this->buf + this->buf_begin, amount); }
+      if (str_size != nullptr) { *str_size += amount; }
+    }
+
+    inline void remove_last_element(string *str, size_t *str_size) {
+      if (str != nullptr) { str->pop_back(); }
+      if (str_size != nullptr) { *str_size -= 1; }
     }
 };
 
